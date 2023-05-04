53c59df9cc496e475d3f77fb15ae1b283570bb33
lijing <lhjhi20@126.com>
2023-05-04 22:31:49 +0800

    submenu bug fix

--- src/components/analysis/result/AnalysisView.vue
+++ src/components/analysis/result/AnalysisView.vue
@@ -1286,10 +1286,17 @@ export default {
         tempDisabledMenuObj['analysis_view_template'] = true // 禁用按钮
         tempDisabledMenuObj['analysis_view_clean'] = true // 禁用按钮
         this.$store.commit('IS_DISABLED_MENU', tempDisabledMenuObj) // 暂时存储需要修改二级菜单的值
-        this.$bus.$emit('openHeaderMenuItem', 'analysis_view', {}, tempDisabledMenuObj) // 修改头部显示效果
+        // 控制二级菜单
+        if (this.activeTabName === 'flightRecordView') {
+          this.$bus.$emit('openHeaderMenuItem', 'analysis_view', {}, tempDisabledMenuObj) // 修改头部显示效果
+        } else if (this.activeTabName === '2dChart') {
+          this.$bus.$emit('openHeaderMenuItem', 'analysis_chart', {}, tempDisabledMenuObj) // 修改头部显示效果
+        } else if (this.activeTabName === 'tableDetail') {
+          this.$bus.$emit('openHeaderMenuItem', 'analysis_table', {}, tempDisabledMenuObj) // 修改头部显示效果
+        }
         this.$message({
           type: 'warning',
-          message: '没有航班号，暂时无法查询数据5！'
+          message: '没有航班号，暂时无法查询数据！'
         })
         return false
       }
@@ -1323,8 +1330,15 @@ export default {
         tempDisabledMenuObj['analysis_view_export'] = false // 启用按钮
         tempDisabledMenuObj['analysis_view_template'] = false // 启用按钮
         tempDisabledMenuObj['analysis_view_clean'] = false // 启用按钮
-        that.$store.commit('IS_DISABLED_MENU', tempDisabledMenuObj) // 暂时存储需要修改二级菜单的值
-        that.$bus.$emit('openHeaderMenuItem', 'analysis_view', {}, tempDisabledMenuObj) // 修改头部显示效果
+        that.$store.commit('IS_DISABLED_MENU', tempDisabledMenuObj) // 暂时存储需要修改二级菜单的值// 控制二级菜单
+        // 控制二级菜单
+        if (that.activeTabName === 'flightRecordView') {
+          that.$bus.$emit('openHeaderMenuItem', 'analysis_view', {}, tempDisabledMenuObj) // 修改头部显示效果
+        } else if (that.activeTabName === '2dChart') {
+          that.$bus.$emit('openHeaderMenuItem', 'analysis_chart', {}, tempDisabledMenuObj) // 修改头部显示效果
+        } else if (that.activeTabName === 'tableDetail') {
+          that.$bus.$emit('openHeaderMenuItem', 'analysis_table', {}, tempDisabledMenuObj) // 修改头部显示效果
+        }
         that.exploreData = getToExplorerValue.data.result.data
         that.exploreTimeData = getToExplorerValueByTime.data.result.data
         that.$store.commit('HIDE_LOADING', '拼命加载中！') // 隐藏加载框
@@ -1335,9 +1349,16 @@ export default {
         tempDisabledMenuObj['analysis_view_export'] = true // 禁用按钮
         tempDisabledMenuObj['analysis_view_template'] = true // 禁用按钮
         tempDisabledMenuObj['analysis_view_clean'] = true // 禁用按钮
-        that.$store.commit('IS_DISABLED_MENU', tempDisabledMenuObj) // 暂时存储需要修改二级菜单的值
-        that.$bus.$emit('openHeaderMenuItem', 'analysis_view', {}, tempDisabledMenuObj) // 修改头部显示效果
-        this.flightChart = ''
+        that.$store.commit('IS_DISABLED_MENU', tempDisabledMenuObj) // 暂时存储需要修改二级菜单的值// 控制二级菜单
+        // 控制二级菜单
+        if (that.activeTabName === 'flightRecordView') {
+          that.$bus.$emit('openHeaderMenuItem', 'analysis_view', {}, tempDisabledMenuObj) // 修改头部显示效果
+        } else if (that.activeTabName === '2dChart') {
+          that.$bus.$emit('openHeaderMenuItem', 'analysis_chart', {}, tempDisabledMenuObj) // 修改头部显示效果
+        } else if (that.activeTabName === 'tableDetail') {
+          that.$bus.$emit('openHeaderMenuItem', 'analysis_table', {}, tempDisabledMenuObj) // 修改头部显示效果
+        }
+        that.flightChart = ''
         // that.$message.error('请求响应失败，请稍后重试！')
         console.log('getCompnentAllHandle##@@##:  ' + err.message)
       })
@@ -2339,7 +2360,6 @@ export default {
       }
     },
     closeFdvDialog (type) {
-      console.log(type, 'type---test')
       if (type) {
         this.dataCleanDiaShow = false
       } else {
